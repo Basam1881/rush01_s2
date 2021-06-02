db1fc700877aeeb974309e5b5f15fe9287f5fe76
Bassam Naji <bnaji@lab1r2s1.42abudhabi.ae>
2021-06-02 09:05:30 +0400

    first

---
+++ main.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bnaji <bnaji@student.42abudhabi.ae>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/02 08:35:28 by bnaji             #+#    #+#             */
+/*   Updated: 2021/06/02 08:35:38 by bnaji            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef UNISTD_H
+# define UNISTD_H
+# include <unistd.h>
+#endif
+
+int		is_valid_condition(char *cond);
+int		rush(void);
+
+int		main(int argc, char *argv[])
+{
+	if (argc != 2 || !(is_valid_condition(argv[1]) && rush()))
+		write(1, "Error\n", 6);
+	return (0);
+}
